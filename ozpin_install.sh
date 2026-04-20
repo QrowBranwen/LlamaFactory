bc0bf4447cbeb13543a3fa4d371b84ba23eea403
ozpintang <ozpintang@tencent.com>
2026-04-20 14:54:55 +0800

    UPDATE CONFIG

--- ozpin_install.sh
+++ ozpin_install.sh
@@ -7,8 +7,10 @@ pip install -e .
 echo "==> [2/4] Installing DeepSpeed ..."
 pip install -r requirements/deepspeed.txt
 
-echo "==> [3/4] Installing Flash Attention 2 ..."
-pip install flash-attn --no-build-isolation
+# echo "==> [3/4] Installing Flash Attention 2 ..."
+# pip install flash-attn --no-build-isolation
+echo "==> [3/4] Installing coscmd ..."
+pip install coscmd
 
 echo "==> [4/4] Installing WandB ..."
 pip install wandb
