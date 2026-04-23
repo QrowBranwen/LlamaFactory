152f23b68b067be25b2638e5726fb1ccad6af2a6
ozpintang <ozpintang@tencent.com>
2026-04-23 23:45:48 +0800

    update config

--- taiji_install.sh
+++ taiji_install.sh
@@ -5,7 +5,8 @@ echo "==> [1/4] Installing LlamaFactory ..."
 pip install -e .
 
 echo "==> [2/4] Installing DeepSpeed ..."
-pip install -r requirements/deepspeed.txt
+# pip install -r requirements/deepspeed.txt
+pip install deepspeed==0.15.4
 
 echo "==> [3/4] Installing Flash Attention 2 ..."
 pip install flash-attn --no-build-isolation
