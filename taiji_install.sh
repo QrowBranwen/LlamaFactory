0e545a8ddec9ce8003fa55109f10c48d87559dc4
ozpintang <ozpintang@tencent.com>
2026-04-23 17:52:58 +0800

    UPDATE CONFIG

---
+++ taiji_install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+echo "==> [1/4] Installing LlamaFactory ..."
+pip install -e .
+
+echo "==> [2/4] Installing DeepSpeed ..."
+pip install -r requirements/deepspeed.txt
+
+echo "==> [3/4] Installing Flash Attention 2 ..."
+pip install flash-attn --no-build-isolation
+# echo "==> [3/4] Installing coscmd ..."
+# pip install coscmd
+
+# echo "==> [4/4] Installing WandB ..."
+# pip install wandb
+
+echo "==> All done!"
