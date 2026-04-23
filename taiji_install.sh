2c99ea99c1e4992e0ac09328dd8452c1fd955404
ozpintang <ozpintang@tencent.com>
2026-04-23 21:47:32 +0800

    UPDATE CONFIG

--- taiji_install.sh
+++ taiji_install.sh
@@ -13,6 +13,6 @@ pip install flash-attn --no-build-isolation
 # pip install coscmd
 
 echo "==> [4/4] Installing tensorboard ..."
-pip install pip install tensorboard
+pip install tensorboard
 
 echo "==> All done!"
