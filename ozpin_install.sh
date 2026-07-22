90215c4c3a1d3806b310a3e808667c8024628c79
ozpintang <ozpintang@tencent.com>
2026-07-22 15:00:43 +0800

    chore: 使用python -m pip替代pip执行安装
    添加CUDA 12.6和12.8的PyTorch安装参考注释

--- ozpin_install.sh
+++ ozpin_install.sh
@@ -2,17 +2,22 @@
 set -e
 
 echo "==> [1/4] Installing LlamaFactory ..."
-pip install -e .
+python -m pip install -e .
 
 echo "==> [2/4] Installing DeepSpeed ..."
-pip install -r requirements/deepspeed.txt
+python -m pip install -r requirements/deepspeed.txt
 
 # echo "==> [3/4] Installing Flash Attention 2 ..."
 # pip install flash-attn --no-build-isolation
 echo "==> [3/4] Installing coscmd ..."
-pip install coscmd
+python -m pip install coscmd
 
 echo "==> [4/4] Installing WandB ..."
-pip install wandb
+python -m pip install wandb
+
+# CUDA 12.6
+# python -m pip install torch==2.10.0 torchvision==0.25.0 torchaudio==2.10.0 --index-url https://download.pytorch.org/whl/cu126
+# CUDA 12.8
+# python -m pip install torch==2.10.0 torchvision==0.25.0 torchaudio==2.10.0 --index-url https://download.pytorch.org/whl/cu128
 
 echo "==> All done!"
